08d868884ccd4e11e465f843ed905bbdd5672681
APCS3rd <APCS3rd@MHS-LT9-14>
2019-08-20 10:12:32 -0700

    Complete Hello World Project

---
+++ Hello World Project/src/Helloworld.java
@@ -0,0 +1,19 @@
+//Dennis Ly
+//August 19,2019
+//First class (iconic) in Java for APCS A
+/*
+ * Prints a message on the screen
+ */
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out. println("Hello World");
+		System.out. println("Hello World");
+		System.out. println("Hello World");
+		System.out. println("Hello World");
+		System.out. println("Hello World");
+
+	}
+
+}
